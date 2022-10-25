d15ca3fa2cf950ad27a84ffad47684290418b8be
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-10-25 19:53:24 -0300

    archetypes e fighter

---
+++ src/Archetypes/index.ts
@@ -0,0 +1,13 @@
+import Archetype from './Archetype';
+import Mage from './Mage';
+import Warrior from './Warrior';
+import Necromancer from './Necromancer';
+import Ranger from './Ranger';
+
+export default Archetype;
+export {
+  Mage,
+  Warrior,
+  Necromancer,
+  Ranger,
+};
